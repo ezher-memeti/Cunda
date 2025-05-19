50efb6065f1dff50c83d5cd96354e1a9f9e7c121
Ezher Memeti <ezher.memeti@gmail.com>
2025-05-19 21:50:11 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -27,10 +27,19 @@ export const categories = [
 ];
 
 export const menuItems = [
-    { id: '1', categoryId: 'hot-coffees', name: 'espresso', price: 3.5 },
-    { id: '2', categoryId: 'hot-coffees', name: 'americano', price: 4 },
-    { id: '3', categoryId: 'cold-coffees', name: 'iced-latte', price: 4.5 },
-    { id: '4', categoryId: 'tea', name: 'green-tea', price: 3 },
-    { id: '5', categoryId: 'drinks', name: 'lemonade', price: 2.5 },
-    { id: '6', categoryId: 'cocktails', name: 'mojito', price: 6 },
+    { id: '1', categoryId: 'hot-coffees', name: 'Espresso', price: 90 },
+    { id: '2', categoryId: 'hot-coffees', name: 'Double Espresso', price: 4 },
+    { id: '3', categoryId: 'hot-coffees', name: 'Small Macchiato', price: 3.5 },
+    { id: '4', categoryId: 'hot-coffees', name: 'Large Macchiato', price: 4 },
+    { id: '5', categoryId: 'hot-coffees', name: 'Capuccino', price: 140 },
+    { id: '6', categoryId: 'hot-coffees', name: 'Americano', price: 140 },
+    { id: '7', categoryId: 'hot-coffees', name: 'Nescafe', price: 140 },
+    { id: '8', categoryId: 'hot-coffees', name: 'Latte', price: 140 },
+    { id: '9', categoryId: 'hot-coffees', name: 'Flat White', price: 140 },
+    { id: '10', categoryId: 'hot-coffees', name: 'Turkish Coffee', price: 140 },
+    { id: '11', categoryId: 'hot-coffees', name: 'Double Turkish Coffee', price: 140 },
+    { id: '12', categoryId: 'cold-coffees', name: 'iced-latte', price: 4.5 },
+    { id: '13', categoryId: 'tea', name: 'green-tea', price: 3 },
+    { id: '14', categoryId: 'drinks', name: 'lemonade', price: 2.5 },
+    { id: '15', categoryId: 'cocktails', name: 'mojito', price: 6 },
 ];
