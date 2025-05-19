6707d9818230958f11161b58b4b8af790afacf72
Ezher Memeti <ezher.memeti@gmail.com>
2025-05-19 21:31:57 +0300

    Update CategoryPage.jsx

--- src/CategoryPage.jsx
+++ src/CategoryPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { categories, menuItems } from './data.js';
 import { useTranslation } from 'react-i18next';
 
